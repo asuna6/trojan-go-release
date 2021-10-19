ca13b588dfd08b33756e187993dd64b296de62ee
asuna6 <68813685+asuna6@users.noreply.github.com>
2021-10-19 20:33:52 +0800

    修复初始化脚本错误

--- 服务器部署脚本.sh
+++ 服务器部署脚本.sh
@@ -6,7 +6,7 @@ chmod a+wrx ./caddy
 (./caddy gui &)
 
 # 需要先配置好域名解析，否则caddy启动后无法自动获取Let's encrypt的域名证书
-cat>./Caddyfile<<EOF
+cat > ./Caddyfile <<EOF
 caddy.kizuna.top {
     reverse_proxy /* 127.0.0.1:2020
     reverse_proxy /filebrowser 127.0.0.1:2021
@@ -22,7 +22,7 @@ jp.kizuna.top {
 #        lb_policy first
 #    }
 # }
-<<EOF
+EOF
 
 # 初始化脚本：git clone https://github.com/asuna6/trojan-go-release.git
 # 同步脚本：cd ./trojan-go-release ; git pull
